fd4d3994376f7e9fc4350ceb534283593ba3e342
Ivan Vegner <ivanvegner@gmail.com>
2017-11-04 18:14:59 -0400

    Comparison to first agent at last eval

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ GAMES_PER_EPISODE = 100
 REPLAY_EVERY_N_GAMES = 64
 BATCH_SIZE = REPLAY_EVERY_N_GAMES
 N_ACTIONS = 7
-EVAL_EVERY_N_EPISODES = 10
+EVAL_EVERY_N_EPISODES = 5
 
 def run_episode(agents):
     emulator = Emulator()
@@ -47,11 +47,11 @@ def run_episode(agents):
         wrappers = []
         player_info = {}
         for i, agent in enumerate(agents):
-            if temp_final_state:
-                for seat in temp_final_state:
-                    player_info[seat.uuid] = {'name': 'Player ' + str(seat.uuid), 'stack': seat.stack if seat.stack else STACK_SIZE}
-            else:
-                player_info[i] = {'name': 'Player ' + str(i), 'stack': STACK_SIZE}
+            # if temp_final_state:
+            #     for seat in temp_final_state:
+            #         player_info[seat.uuid] = {'name': 'Player ' + str(seat.uuid), 'stack': seat.stack if seat.stack else STACK_SIZE}
+            # else:
+            player_info[i] = {'name': 'Player ' + str(i), 'stack': STACK_SIZE}
             wrappers.append(DQNAgentWrapper(agent))
             emulator.register_player(uuid=i, player=wrappers[-1])
         emulator.set_game_rule(N_AGENTS, 2, BB_SIZE / 2, 0)
@@ -88,6 +88,7 @@ if __name__ == '__main__':
     _sample_features = DQNAgent(3, 3).make_features(SAMPLE_ACTIONS, SAMPLE_HOLE_CARDS, SAMPLE_STATE)
     STATE_SIZE = len(_sample_features)
 
+    oldest_agents = [DQNAgent(STATE_SIZE, N_ACTIONS)] * N_AGENTS
     old_agents = [DQNAgent(STATE_SIZE, N_ACTIONS)] * N_AGENTS
     agents = [DQNAgent(STATE_SIZE, N_ACTIONS)] * N_AGENTS
 
@@ -102,7 +103,14 @@ if __name__ == '__main__':
                 highest_idx = seat.uuid
         new_agents[highest_idx].model.reset_states()  # clear memory of RNN
 
-        if e % EVAL_EVERY_N_EPISODES == 0 or e == N_EPISODES-1: # run 3x old versions against 1 new version
+        if e == N_EPISODES-1:
+            print('====')
+            print('Evaluating')
+            _, final_state, winner_counts, n_games_played = run_episode([agents[0]] + oldest_agents[:-1])
+            print('\nNew won against old {} percent of games'.format((winner_counts[0] / n_games_played) * 100))
+            print('====')
+
+        elif e % EVAL_EVERY_N_EPISODES == 0: # run 3x old versions against 1 new version
             print('====')
             print('Evaluating')
             _, final_state, winner_counts, n_games_played = run_episode([agents[0]] + old_agents[:-1])
@@ -111,6 +119,6 @@ if __name__ == '__main__':
             old_agents = agents
 
         agents = [new_agents[highest_idx]] * N_AGENTS
-        print(highest_roll)
+
 
     agents[0].save('pokerai_rl.h5')
