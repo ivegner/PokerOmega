3f1484124e25d893de8ff6cc6b450c8b0157ae22
Zach Schaffer <cursedhydra@gmail.com>
2017-11-04 16:07:49 -0400

    the rest of the fucking owl

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#pylint:disable=C0103
+# pylint:disable=C0103
 '''Poker simulation environment
 Controls:
 Make 4*current best model (random if first episode)
@@ -24,17 +24,18 @@ from agent.dqn_agent import DQNAgent
 from environment.sample_state import SAMPLE_STATE, SAMPLE_HOLE_CARDS, SAMPLE_ACTIONS
 from environment.dqn_agent_wrapper import DQNAgentWrapper
 from pypokerengine.api.emulator import Emulator
+from pypokerengine.utils.game_state_utils import attach_hole_card_from_deck
+import numpy as np
 
 N_AGENTS = 4
 BB_SIZE = 10
 STACK_SIZE = 200
-N_EPISODES = 1
-GAMES_PER_EPISODE = 1
+N_EPISODES = 10
+GAMES_PER_EPISODE = 2
 REPLAY_EVERY_N_GAMES = 32
 BATCH_SIZE = REPLAY_EVERY_N_GAMES
 N_ACTIONS = 7
 
-emulator = Emulator()
 
 # used only for calculating # of features
 _sample_features = DQNAgent(3, 3).make_features(SAMPLE_ACTIONS, SAMPLE_HOLE_CARDS, SAMPLE_STATE)
@@ -43,23 +44,27 @@ STATE_SIZE = len(_sample_features)
 agents = [DQNAgent(STATE_SIZE, N_ACTIONS)] * N_AGENTS
 
 for e in range(N_EPISODES):
+    emulator = Emulator()
     temp_final_state = {}
     for game in range(GAMES_PER_EPISODE):
         wrappers = []
         player_info = {}
         for i, agent in enumerate(agents):
             if temp_final_state:
-                for seat in temp_final_state['seats']:
-                    if seat['uuid'] == i:
-                        player_info[i] = {'name': 'Player ' + i, 'stack': seat['stack']}
-                        break
+                for seat in temp_final_state:
+                    player_info[seat.uuid] = {'name': 'Player ' + str(seat.uuid), 'stack': seat.stack if seat.stack else STACK_SIZE}
             else:
-                player_info[i] = {'name': 'Player ' + i, 'stack': STACK_SIZE}
+                player_info[i] = {'name': 'Player ' + str(i), 'stack': STACK_SIZE}
             wrappers.append(DQNAgentWrapper(agent))
             emulator.register_player(uuid=i, player=wrappers[-1])
-
+        emulator.set_game_rule(N_AGENTS, 999, BB_SIZE / 2, 0)
         initial_game_state = emulator.generate_initial_game_state(player_info)
-        game_finish_state, events = emulator.run_until_game_finish(initial_game_state)
+        #for uuid in range(len(agents)):
+        #    initial_game_state = attach_hole_card_from_deck(initial_game_state,uuid)
+        #initial_game_state['next_player'] = np.random.randint(0,N_AGENTS+1)
+        game_state, events = emulator.start_new_round(initial_game_state)
+        game_finish_state, events = emulator.run_until_game_finish(game_state)
+
 
         # state = env.reset()
         # done = False
@@ -72,18 +77,19 @@ for e in range(N_EPISODES):
         #     agents[current_player_idx] = current_agent
         #     state = next_state
         agents[:] = [wrapper.agent for wrapper in wrappers]
-        temp_final_state = wrappers[0].final_state
+        temp_final_state = game_finish_state['table'].seats.players
 
-        if game % REPLAY_EVERY_N_GAMES == 0 or game == GAMES_PER_EPISODE-1:
+        if game % REPLAY_EVERY_N_GAMES == 0 or game == GAMES_PER_EPISODE - 1:
             # replay memory for every agent
             map(lambda a: a.replay(BATCH_SIZE), agents)
         print('Episode {} over'.format(e))
 
     # Pick best model
     highest_roll, highest_idx = 0, None
-    for seat in temp_final_state['seats']:
-        if seat['stack'] > highest_roll:
-            highest_roll = seat['stack']
-            highest_idx = seat['uuid']
-    agents[highest_idx].reset_states()  # clear memory of RNN
+    for seat in temp_final_state:
+        if seat.stack > highest_roll:
+            highest_roll = seat.stack
+            highest_idx = seat.uuid
+    agents[highest_idx].model.reset_states()  # clear memory of RNN
     agents = [agents[highest_idx]] * N_AGENTS
+    print(highest_roll)
