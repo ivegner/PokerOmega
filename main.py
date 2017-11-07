00856f04334292bf9a2fd0e7ecad9337a778b95d
Zach Schaffer <cursedhydra@gmail.com>
2017-11-07 10:58:29 -0500

    Merge remote-tracking branch 'refs/remotes/origin/master'
    Conflicts:
    	.gitignore
    	agent/dqn_agent.py
    	main.py

--- main.py
+++ main.py
@@ -26,10 +26,15 @@ from environment.dqn_agent_wrapper import DQNAgentWrapper
 from pypokerengine.api.emulator import Emulator
 import numpy as np
 import sys
+<<<<<<< HEAD
+=======
+import argparse
+>>>>>>> master
 import copy
 
 np.random.seed(12)
 
+<<<<<<< HEAD
 N_AGENTS = 4
 BB_SIZE = 10
 STACK_SIZE = 200
@@ -43,6 +48,42 @@ USE_ROLL_INSTEAD_OF_WIN_COUNT = False
 PERSISTENT_STACKS = False
 EVAL_AGAINST_RANDOM = False  # False = evaluates against older version (EVAL_EVERY_N_EPISODES episodes older)
 
+=======
+def parse_cli():
+    parser = argparse.ArgumentParser(description='Train a PokerOmega instance with the given parameters')
+    parser.add_argument('--agents', '-a', default='4', dest='n_agents',type=int, help = '(default: %(default)s)')
+    parser.add_argument('--games', '-g', default='100', dest='games_per_episode', type=int, help = '(default: %(default)s)')
+    parser.add_argument('--replay-every', '-r', dest='replay_every', default='20', type=int, help = '(default: %(default)s)')
+    parser.add_argument('--episodes', '-e', dest='n_episodes', default='30', type=int, help = '(default: %(default)s)')
+    parser.add_argument('--epsilon-min', '--emin', dest='e_min', default='0.01', type=float, help = '(default: %(default)s)')
+    parser.add_argument('--epsilon-decay', '--edec', dest='e_decay', default='0.995', type=float, help = '(default: %(default)s)')
+    parser.add_argument('--gamma', default='0.95', dest='gamma', type=float, help = '(default: %(default)s)')
+    parser.add_argument('--eval-every', '--eval', dest='eval_every', default='10', type=int, help = '(default: %(default)s)')
+    parser.add_argument('--eval-against-random', '--random', dest='random_eval', default=False, action='store_true', help = '(default: %(default)s)')
+    parser.add_argument('--output', '-o', dest='output_filename', default=None,
+                        help='IF NOT SET, WILL NOT SAVE TRAINED MODEL. Automatically appends ".h5"')
+    return parser.parse_args()
+
+# Parse CLI args
+args = parse_cli()
+STATE_SIZE = 134
+BB_SIZE = 10
+STACK_SIZE = 200
+N_ACTIONS = 8
+USE_ROLL_INSTEAD_OF_WIN_COUNT = False
+PERSISTENT_STACKS = False
+
+N_AGENTS = args.n_agents
+N_EPISODES = args.n_episodes
+GAMES_PER_EPISODE = args.games_per_episode
+REPLAY_EVERY_N_GAMES = args.replay_every
+BATCH_SIZE = REPLAY_EVERY_N_GAMES
+EVAL_EVERY_N_EPISODES = args.eval_every
+EVAL_AGAINST_RANDOM = args.random_eval  # False = evaluates against older version (EVAL_EVERY_N_EPISODES episodes older)
+E_MIN = args.e_min
+E_DECAY = args.e_decay
+GAMMA = args.gamma
+>>>>>>> master
 
 def run_episode(agents):
     emulator = Emulator()
@@ -121,6 +162,7 @@ def copy_agent(agent):
     return copied
     #return agent
 
+<<<<<<< HEAD
 if __name__ == '__main__':
     # used only for calculating # of features
     _sample_features = DQNAgent(3, 3, N_AGENTS).make_features(SAMPLE_ACTIONS, SAMPLE_HOLE_CARDS, SAMPLE_STATE)
@@ -162,12 +204,69 @@ if __name__ == '__main__':
         if EVAL_AGAINST_RANDOM:
             if e == N_EPISODES - 1 or e % EVAL_EVERY_N_EPISODES == 0:
                 if e != 0:
+=======
+def make_random_agents():
+    return [DQNAgent(STATE_SIZE, N_ACTIONS, N_AGENTS, E_MIN, E_DECAY, GAMMA)] * N_AGENTS
+
+# # used only for calculating # of features
+# _sample_features = DQNAgent(3, 3, N_AGENTS).make_features(SAMPLE_ACTIONS, SAMPLE_HOLE_CARDS, SAMPLE_STATE)
+# STATE_SIZE = len(_sample_features)
+
+oldest_agents = make_random_agents()
+old_agents = make_random_agents()
+agents = make_random_agents()
+
+if len(sys.argv) >= 3 and sys.argv[1] == '-l':  # load provided filename as weights
+    for a in agents:
+        a.load(sys.argv[2])
+
+hyperparam_list = {'games_per_episode': GAMES_PER_EPISODE, 'replay': REPLAY_EVERY_N_GAMES, 'n_episodes': N_EPISODES,
+                    'n_agents': N_AGENTS, 'epsilon_min': agents[0].epsilon_min,
+                    'epsilon_decay': agents[0].epsilon_decay,
+                    'gamma': agents[0].gamma}
+print(hyperparam_list)
+
+for e in range(N_EPISODES):
+    oldest_agents = make_random_agents()
+    new_agents, final_state, winner_counts, n_games_played = run_episode(agents)
+
+    print('\nEpisode {} over'.format(e))
+    # Pick best model
+    highest_idx = None
+    if USE_ROLL_INSTEAD_OF_WIN_COUNT:
+        highest_roll = 0
+        for seat in final_state:
+            if seat.stack > highest_roll:
+                highest_roll = seat.stack
+                highest_idx = seat.uuid
+    else:
+        highest_idx = np.argmax(winner_counts)
+    #new_agents[highest_idx].model.reset_states()# clear memory of RNN
+    best_current_agent = copy_agent(new_agents[highest_idx])
+    agents = [copy_agent(best_current_agent)] * N_AGENTS
+
+    if EVAL_AGAINST_RANDOM:
+        if e == N_EPISODES - 1 or e % EVAL_EVERY_N_EPISODES == 0:
+            if e != 0:
+                print('====')
+                print('Final evaluation')
+                _, final_state, winner_counts, n_games_played = run_episode([best_current_agent] + oldest_agents[:-1])
+                print('\nNewest best won against oldest {} percent of games'.format(
+                    (winner_counts[0] / n_games_played) * 100))
+                print('====')
+
+    else:
+        if e == N_EPISODES - 1 or e % EVAL_EVERY_N_EPISODES == 0:  # run 3x old versions against 1 new version
+            if e != 0:
+                if e % 100 == 0:
+>>>>>>> master
                     print('====')
                     print('Final evaluation')
                     _, final_state, winner_counts, n_games_played = run_episode([best_current_agent] + oldest_agents[:-1])
                     print('\nNewest best won against oldest {} percent of games'.format(
                         (winner_counts[0] / n_games_played) * 100))
                     print('====')
+<<<<<<< HEAD
 
         else:
             if e == N_EPISODES - 1 or e % EVAL_EVERY_N_EPISODES == 0:  # run 3x old versions against 1 new version
@@ -191,3 +290,18 @@ if __name__ == '__main__':
                             old_agents[agent_idx] = copy_agent(best_current_agent)
 
     agents[0].save('pokerai_rl.h5')
+=======
+                else:
+                    print('====')
+                    print('Evaluating')
+                    _, final_state, winner_counts, n_games_played = run_episode([best_current_agent] + old_agents[:-1])
+                    print('\nNew won against old {} percent of games'.format(
+                        (winner_counts[0] / n_games_played) * 100))
+                    print('====')
+
+                    for agent_idx in range(N_AGENTS):
+                        old_agents[agent_idx] = copy_agent(best_current_agent)
+
+if args.output_filename:
+    agents[0].save(args.output_filename + '.h5')
+>>>>>>> master
